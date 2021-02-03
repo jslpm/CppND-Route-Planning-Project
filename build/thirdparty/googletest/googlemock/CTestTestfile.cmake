78c76501806617e8b0a02282bfaab6c5cabb5911
José Luis Pascal <jose.pascal@gmail.com>
2021-02-03 13:20:09 -0300

    initial commit

---
+++ build/thirdparty/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/argosx/Documents/codes/cpp/udacity-cpp-nanodegree/001-introduction/project1/CppND-Route-Planning-Project/thirdparty/googletest/googlemock
+# Build directory: /home/argosx/Documents/codes/cpp/udacity-cpp-nanodegree/001-introduction/project1/CppND-Route-Planning-Project/build/thirdparty/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
