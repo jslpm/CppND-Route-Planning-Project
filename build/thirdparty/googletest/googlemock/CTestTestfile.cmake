3d1c7f3300b4c287af044ef8b303d723314fd6c7
José Luis Pascal <jose.pascal@gmail.com>
2021-02-04 21:09:53 -0300

    delete build folder

--- build/thirdparty/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/argosx/Documents/codes/cpp/udacity-cpp-nanodegree/001-introduction/project1/CppND-Route-Planning-Project/thirdparty/googletest/googlemock
-# Build directory: /home/argosx/Documents/codes/cpp/udacity-cpp-nanodegree/001-introduction/project1/CppND-Route-Planning-Project/build/thirdparty/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
